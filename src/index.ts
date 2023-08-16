d6cf7a944e8a7952c5947721be96319312e32d12
LoTwT <709937065@qq.com>
2023-08-16 13:12:08 +0800

    feat: vite-plugin-sirv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-export const one = 1
+import type { Plugin } from "vite"
+import type { Options as SirvOptions } from "sirv"
+import sirv from "sirv"
+
+export interface VitePluginSirvConfig {
+  dir: string
+  sirvOptions?: SirvOptions
+}
+
+export default function (config: VitePluginSirvConfig): Plugin {
+  let resolvedSirvOptions = config.sirvOptions ?? {}
+
+  return {
+    name: "vite-plugin-sirv",
+
+    configResolved: (config) => {
+      resolvedSirvOptions = {
+        ...resolvedSirvOptions,
+        dev: config.mode === "serve",
+      }
+    },
+
+    configureServer: (server) => {
+      return () => {
+        server.middlewares.use(sirv(config.dir, resolvedSirvOptions))
+      }
+    },
+  }
+}
